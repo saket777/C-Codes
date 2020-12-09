4617403ffd3643d0b1a9e9da2c9d3b67e972d4af
Saket Shubham <46522344+saket777@users.noreply.github.com>
2020-12-09 20:07:01 +0530

    Add files via upload

---
+++ Print an integer.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int number;
+    printf("Enter an Integer:");
+    scanf("%d",& number);
+    printf("You intered the integer:%d",number);
+    return 0;
+}
---
+++ Sum of two numbers.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    printf("Enter the Two numbers:");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("Sum of two Numbers is:%d",c);
+    return 0;
+    
+}
